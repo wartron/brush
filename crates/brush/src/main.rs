235496fdd6316c4aaa2b334b4c3c898412bc5673
ArthurBrussee <arthur.brussee@gmail.com>
2024-06-06 12:05:38 +0100

    Conditional spawn native/web UI

--- crates/brush/src/main.rs
+++ crates/brush/src/main.rs
@@ -13,12 +13,12 @@ mod train;
 mod utils;
 mod viewer;
 
+use eframe::egui_wgpu::WgpuConfiguration;
 use std::sync::Arc;
+use viewer::Viewer;
 
-use eframe::{egui_wgpu::WgpuConfiguration, NativeOptions};
 #[cfg(feature = "tracy")]
 use tracing_subscriber::layer::SubscriberExt;
-use viewer::Viewer;
 
 fn main() -> anyhow::Result<()> {
     #[cfg(feature = "tracy")]
@@ -26,30 +26,63 @@ fn main() -> anyhow::Result<()> {
         tracing_subscriber::registry().with(tracing_tracy::TracyLayer::default()),
     )?;
 
-    // Build app display.
-    let native_options = NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size(egui::Vec2::new(1280.0, 720.0))
-            .with_active(true),
-        vsync: false,
-        // Need a slightly more careful wgpu init to support burn.
-        wgpu_options: WgpuConfiguration {
-            device_descriptor: Arc::new(|adapter| wgpu::DeviceDescriptor {
-                label: Some("egui+burn wgpu device"),
-                required_features: wgpu::Features::default(),
-                required_limits: adapter.limits(),
-            }),
-            ..Default::default()
-        },
+    let wgpu_config = WgpuConfiguration {
+        device_descriptor: Arc::new(|adapter| wgpu::DeviceDescriptor {
+            label: Some("egui+burn wgpu device"),
+            required_features: wgpu::Features::default(),
+            required_limits: adapter.limits(),
+        }),
+        supported_backends: wgpu::Backends::PRIMARY,
         ..Default::default()
     };
 
-    eframe::run_native(
-        "Brush 🖌️",
-        native_options,
-        Box::new(move |cc| Box::new(Viewer::new(cc))),
-    )
-    .unwrap();
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        // Build app display.
+        let native_options = NativeOptions {
+            viewport: egui::ViewportBuilder::default()
+                .with_inner_size(egui::Vec2::new(1280.0, 720.0))
+                .with_active(true),
+            vsync: false,
+            // Need a slightly more careful wgpu init to support burn.
+            wgpu_options: WgpuConfiguration {
+                device_descriptor: Arc::new(|adapter| wgpu::DeviceDescriptor {
+                    label: Some("egui+burn wgpu device"),
+                    required_features: wgpu::Features::default(),
+                    required_limits: adapter.limits(),
+                }),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        eframe::run_native(
+            "Brush 🖌️",
+            native_options,
+            Box::new(move |cc| Box::new(Viewer::new(cc))),
+        )
+        .unwrap();
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
+
+        let web_options = eframe::WebOptions {
+            wgpu_options: wgpu_config,
+            ..Default::default()
+        };
+
+        wasm_bindgen_futures::spawn_local(async {
+            eframe::WebRunner::new()
+                .start(
+                    "main_canvas", // hardcode it
+                    web_options,
+                    Box::new(|cc| Box::new(Viewer::new(cc))),
+                )
+                .await
+                .expect("failed to start eframe");
+        });
+    }
 
     Ok(())
 }
