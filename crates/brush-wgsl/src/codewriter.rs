cf77b915a88f6fca15aa6cd5f6029c34a1ae8319
ArthurBrussee <arthur.brussee@gmail.com>
2024-06-25 22:40:12 +0100

    Allow code write any AsRef str (eg. a Vec<String>)

--- crates/brush-wgsl/src/codewriter.rs
+++ crates/brush-wgsl/src/codewriter.rs
@@ -11,17 +11,17 @@ impl CodeWriter {
         }
     }
 
-    pub(crate) fn add_line(&mut self, line: &str) {
-        self.indent -= line.chars().filter(|&c| c == '}').count();
+    pub(crate) fn add_line<T: AsRef<str>>(&mut self, line: T) {
+        self.indent -= line.as_ref().chars().filter(|&c| c == '}').count();
         for _ in 0..4 * self.indent {
             self.state.push(' ');
         }
-        self.state += line;
+        self.state += line.as_ref();
         self.state += "\n";
-        self.indent += line.chars().filter(|&c| c == '{').count();
+        self.indent += line.as_ref().chars().filter(|&c| c == '{').count();
     }
 
-    pub(crate) fn add_lines(&mut self, lines: &[&str]) {
+    pub(crate) fn add_lines<T: AsRef<str>>(&mut self, lines: &[T]) {
         for line in lines {
             self.add_line(line);
         }
