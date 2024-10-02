2b1de90af01c7c716efd9e1ef2cd9481960b0b6d
Arthur Brussee <arthur.brussee@gmail.com>
2024-10-02 15:51:34 +0100

    Small fixes to ssim

--- crates/brush-train/src/ssim.rs
+++ crates/brush-train/src/ssim.rs
@@ -1,12 +1,11 @@
 use burn::tensor::{backend::Backend, module::conv2d, ops::ConvOptions, Tensor};
 
 fn gaussian<B: Backend>(window_size: usize, sigma: f32, device: &B::Device) -> Tensor<B, 1> {
-    let sigma2 = 2.0 * sigma.powf(2.0);
     let window_extent = (window_size / 2) as f32;
-    let gauss_floats: Vec<_> = (0..window_size)
-        .map(|x| (-(x as f32 - window_extent).powf(2.0) / sigma2).exp())
+    let vals: Vec<_> = (0..window_size)
+        .map(|x| f32::exp(-(x as f32 - window_extent).powf(2.0) / (2.0 * sigma.powf(2.0))))
         .collect();
-    let gauss = Tensor::from_floats(gauss_floats.as_slice(), device);
+    let gauss = Tensor::from_floats(vals.as_slice(), device);
     gauss.clone() / gauss.sum()
 }
 
@@ -16,8 +15,8 @@ fn create_window<B: Backend>(
     device: &B::Device,
 ) -> Tensor<B, 4> {
     let window1d = gaussian(window_size, 1.5, device).reshape([window_size, 1]);
-    let window2d = window1d.clone() * window1d.transpose();
-    window2d.unsqueeze().repeat_dim(1, channel)
+    let window2d = window1d.clone().matmul(window1d.transpose());
+    window2d.unsqueeze().repeat_dim(0, channel)
 }
 
 pub fn ssim<B: Backend>(
@@ -27,15 +26,13 @@ pub fn ssim<B: Backend>(
 ) -> Tensor<B, 1> {
     let device = &img1.device();
 
-    let [_, height, width, channel] = img1.dims();
+    let [b, _, _, channel] = img1.dims();
+    let window = create_window::<B>(window_size, channel, device);
 
-    let real_size = window_size.min(height).min(width);
-    let window = create_window::<B>(real_size, channel, device);
-
-    let conv_options = ConvOptions::new([1, 1], [0, 0], [0, 0], channel);
+    let padding = window_size / 2;
+    let conv_options = ConvOptions::new([1, 1], [padding, padding], [0, 0], channel);
     let mu1 = conv2d(img1.clone(), window.clone(), None, conv_options.clone());
     let mu2 = conv2d(img2.clone(), window.clone(), None, conv_options.clone());
-
     let mu1_sq = mu1.clone().powf_scalar(2);
     let mu2_sq = mu2.clone().powf_scalar(2);
     let mu1_mu2 = mu1 * mu2;
@@ -46,12 +43,14 @@ pub fn ssim<B: Backend>(
         None,
         conv_options.clone(),
     ) - mu1_sq.clone();
+
     let sigma2_sq = conv2d(
         img2.clone().powf_scalar(2.0),
         window.clone(),
         None,
         conv_options.clone(),
     ) - mu2_sq.clone();
+
     let sigma12 = conv2d(
         img1.clone() * img2.clone(),
         window.clone(),
@@ -59,12 +58,10 @@ pub fn ssim<B: Backend>(
         conv_options.clone(),
     ) - mu1_mu2.clone();
 
-    let c1 = (0.01f32).powf(2.0);
-    let c2 = (0.03f32).powf(2.0);
+    let c1: f32 = 0.01f32.powf(2.0);
+    let c2: f32 = 0.03f32.powf(2.0);
 
-    let v1 = sigma12 * 2.0 + c2;
-    let v2 = sigma1_sq + sigma2_sq + c2;
-
-    let ssim_map = ((mu1_mu2 * 2.0 + c1) * v1) / ((mu1_sq + mu2_sq + c1) * v2);
-    ssim_map.mean()
+    let ssim_map = ((mu1_mu2 * 2.0 + c1) * (sigma12 * 2.0 + c2))
+        / ((mu1_sq + mu2_sq + c1) * (sigma1_sq + sigma2_sq + c2));
+    ssim_map.mean_dim(1).mean_dim(2).mean_dim(3).reshape([b])
 }
