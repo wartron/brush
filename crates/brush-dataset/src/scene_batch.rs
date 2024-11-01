b7dbd12ac2b3dc73b8c3663fe49acb2676f410c9
Arthur <arthur.brussee@gmail.com>
2024-11-01 16:08:07 +0000

    Remove rng shuffle

--- crates/brush-dataset/src/scene_batch.rs
+++ crates/brush-dataset/src/scene_batch.rs
@@ -4,6 +4,7 @@ use brush_train::image::image_to_tensor;
 use brush_train::scene::Scene;
 use brush_train::train::SceneBatch;
 use burn::tensor::Tensor;
+use rand::{Rng, SeedableRng};
 
 use crate::spawn_future;
 
@@ -17,8 +18,9 @@ impl<B: Backend> SceneLoader<B> {
         // The bounded size == number of batches to prefetch.
         let (tx, rx) = async_std::channel::bounded(5);
         let device = device.clone();
-        let len = scene.views.len() as u64;
         let scene_extent = scene.bounds(0.0, 0.0).extent.length() as f64;
+
+        let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
 
         let fut = async move {
             // Nb: Index works as a "seed" to the dataloader.
@@ -26,14 +28,9 @@ impl<B: Backend> SceneLoader<B> {
 
             loop {
                 let indices: Vec<_> = (0..batch_size)
-                    .map(|i| {
-                        miller_shuffle((index + i as u64) % len, (index + i as u64) / len, len)
-                    })
+                    .map(|_| rng.gen_range(0..scene.views.len()))
                     .collect();
-                let gt_views: Vec<_> = indices
-                    .iter()
-                    .map(|&x| scene.views[x as usize].clone())
-                    .collect();
+                let gt_views: Vec<_> = indices.iter().map(|&x| scene.views[x].clone()).collect();
                 let selected_tensors: Vec<_> = gt_views
                     .iter()
                     .map(|view| image_to_tensor(&view.image, &device))
@@ -65,41 +62,4 @@ impl<B: Backend> SceneLoader<B> {
             .await
             .expect("Somehow lost data loading channel!")
     }
-}
-
-// Simple rust port of https://github.com/RondeSC/Miller_Shuffle_Algo/blob/main/MillerShuffle.c,
-// "Miller Shuffle Algorithm E variant".
-// Copyright 2022 Ronald R. Miller
-// http://www.apache.org/licenses/LICENSE-2.0
-fn miller_shuffle(inx: u64, shuffle_id: u64, list_size: u64) -> u64 {
-    let p1 = 24317;
-    let p2 = 32141;
-    let p3 = 63629; // for shuffling 60,000+ indexes
-
-    let shuffle_id = shuffle_id + 131 * (inx / list_size); // have inx overflow effect the mix
-    let mut si = (inx + shuffle_id) % list_size; // cut the deck
-    let r1 = shuffle_id % p1 + 42; // randomizing factors crafted empirically (by automated trial and error)
-    let r2 = ((shuffle_id * 0x89) ^ r1) % p2;
-    let r3 = (r1 + r2 + p3) % list_size;
-    let r4 = r1 ^ r2 ^ r3;
-    let rx = (shuffle_id / list_size) % list_size + 1;
-    let rx2 = (shuffle_id / list_size / list_size) % list_size + 1;
-    // perform conditional multi-faceted mathematical spin-mixing (on avg 2 1/3 shuffle ops done + 2 simple Xors)
-    if si % 3 == 0 {
-        si = (((si / 3) * p1 + r1) % ((list_size + 2) / 3)) * 3;
-    } // spin multiples of 3
-    if si % 2 == 0 {
-        si = (((si / 2) * p2 + r2) % ((list_size + 1) / 2)) * 2;
-    } // spin multiples of 2
-    if si < list_size / 2 {
-        si = (si * p3 + r4) % (list_size / 2);
-    }
-    if (si ^ rx) < list_size {
-        si ^= rx;
-    } // flip some bits with Xor
-    si = (si * p3 + r3) % list_size; // relatively prime gears turning operation
-    if (si ^ rx2) < list_size {
-        si ^= rx2;
-    }
-    si
 }
